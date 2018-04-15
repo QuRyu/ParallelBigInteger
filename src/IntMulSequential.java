9390ed789d8bbc0eb25268a133b876bbc66926c5
TQ24 <yquan20@colby.edu>
2018-04-14 23:45:23 -0400

    sequential yes

--- src/IntMulSequential.java
+++ src/IntMulSequential.java
@@ -2,6 +2,8 @@
 This is the sequential algorithm for computing two 32 bits integer
 Implemented by Tracy and Zilin
 */
+import java.util.*;
+
 
 public class IntMulSequential extends IntMul {
     @Override
@@ -14,13 +16,11 @@ public class IntMulSequential extends IntMul {
       	//binary multiplication
       	for (int a = 0; a < bi_i.length; a++){
         	for (int b = 0; b < bi_j.length; b++){
-            	resultBi[a+b] += bi_i[a] * bi_j[b] % 2;
+            	resultBi[a+b] += bi_i[a] * bi_j[b];
+              resultBi[a+b+1] += resultBi[a+b]/2;
             	resultBi[a+b] %= 2;
-            	resultBi[a+b+1] += resultBi[a+b]/2;
+
         	}
-      	}
-      	for (int k=0;k<resultBi.length ;k++ ) {
-      		System.out.println(resultBi[k]);
       	}
 
       	BigInt result = new BigInt(resultBi);
@@ -30,19 +30,15 @@ public class IntMulSequential extends IntMul {
 
     //test
     public static void main(String[] args) {
-    	BigInt mulL = new BigInt(23231);
-    	BigInt mulR = new BigInt(51431);
+    	BigInt mulL = new BigInt(235);
+    	BigInt mulR = new BigInt(23);
+      System.out.println("Number1: " + mulL.toString());
+    	System.out.println("Number2: " + mulR.toString());
+      System.out.println("Expected result: " + mulL.getInteger()*mulR.getInteger());
     	IntMulSequential test = new IntMulSequential();
-    	BigInt result = test.mul(mulL, mulR); 
-    	// System.out.println(result.toString());
-    	// System.out.println(mulL.toString());
-    	// System.out.println(mulR.toString());
+    	BigInt result = test.mul(mulL, mulR);
+    	System.out.println("Our result: " + result.toString());
     	System.out.println("\n");
-    	for (int i=0;i<result.getInternal().length ;i++ ) {
-    		System.out.println(result.getInternal()[i]);
-    	}
-    	
+
     }
 }
-
-
